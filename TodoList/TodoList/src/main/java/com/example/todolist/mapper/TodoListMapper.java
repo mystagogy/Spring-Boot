42e6eca9bb59d949c7879059ee2d400a275840ee
mystagogy <hi7747@naver.com>
2022-12-03 16:07:16 +0900

    feat: add mybatis mapper
    using maria DB

---
+++ TodoList/TodoList/src/main/java/com/example/todolist/mapper/TodoListMapper.java
@@ -0,0 +1,12 @@
+package com.example.todolist.mapper;
+
+import com.example.todolist.dto.MemberDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface TodoListMapper {
+
+    public int insertMember(MemberDTO memberDTO);
+
+    public String checkMember(String email);
+}
