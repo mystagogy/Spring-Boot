774ce20ca8dad1361ddc4f83f6678c9e3d965080
mystagogy <hi7747@naver.com>
2022-12-03 16:13:26 +0900

    feat: create member repository

---
+++ TodoList/TodoList/src/main/java/com/example/todolist/repository/MemberRepository.java
@@ -0,0 +1,22 @@
+package com.example.todolist.repository;
+
+import com.example.todolist.dto.MemberDTO;
+import com.example.todolist.mapper.TodoListMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberRepository {
+
+    @Autowired
+    TodoListMapper mapper;
+
+    public int insertMember(MemberDTO memberDTO){
+        return mapper.insertMember(memberDTO);
+    }
+
+    public String checkMember(String email){
+        return mapper.checkMember(email);
+    }
+
+}
