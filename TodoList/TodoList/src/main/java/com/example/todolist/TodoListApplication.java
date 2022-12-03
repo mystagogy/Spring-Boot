182188666cb098ce974df2f2c6d0df06e6ee2169
mystagogy <hi7747@naver.com>
2022-12-03 16:11:25 +0900

    feat: add mapper scan

--- TodoList/TodoList/src/main/java/com/example/todolist/TodoListApplication.java
+++ TodoList/TodoList/src/main/java/com/example/todolist/TodoListApplication.java
@@ -1,9 +1,11 @@
 package com.example.todolist;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(value = {"com.example.todolist.mapper"})
 public class TodoListApplication {
 
     public static void main(String[] args) {
