eb52cbab58b933bdb4366006f5074e47a8c0133d
mystagogy <hi7747@naver.com>
2022-12-03 16:13:39 +0900

    feat: create member service

---
+++ TodoList/TodoList/src/main/java/com/example/todolist/service/MemberService.java
@@ -0,0 +1,21 @@
+package com.example.todolist.service;
+
+import com.example.todolist.dto.MemberDTO;
+import com.example.todolist.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+
+    @Autowired
+    MemberRepository mr;
+
+    public int insertMember(MemberDTO memberDTO){
+        return mr.insertMember(memberDTO);
+    }
+
+    public String checkMember(String email){
+        return mr.checkMember(email);
+    }
+}
