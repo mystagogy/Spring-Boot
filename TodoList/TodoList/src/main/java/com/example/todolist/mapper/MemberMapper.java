b9cf1de70cd573ebb7dfd2dfd1d45a37d336363d
mystagogy <hi7747@naver.com>
2022-12-05 15:05:31 +0900

    refactor : refactor member's mapper name

---
+++ TodoList/TodoList/src/main/java/com/example/todolist/mapper/MemberMapper.java
@@ -0,0 +1,14 @@
+package com.example.todolist.mapper;
+
+import com.example.todolist.dto.MemberDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MemberMapper {
+
+    public int insertMember(MemberDTO memberDTO);
+
+    public String checkMember(String email);
+
+    public MemberDTO loginMember(MemberDTO memberDTO);
+}
