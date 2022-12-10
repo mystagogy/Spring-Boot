1bdbb3b7bdafb65abdd8752dcd6e3ffcf3fa4f90
mystagogy <hi7747@naver.com>
2022-12-10 19:22:05 +0900

    Initial Commit

---
+++ DTCoin/DTCoin/src/main/java/com/example/dtcoin/DtCoinApplication.java
@@ -0,0 +1,13 @@
+package com.example.dtcoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtCoinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DtCoinApplication.class, args);
+    }
+
+}
---
+++ DTCoin/DTCoin/src/test/java/com/example/dtcoin/DtCoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dtcoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtCoinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
