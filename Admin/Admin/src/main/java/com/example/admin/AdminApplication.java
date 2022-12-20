84c8619b7ebdaa3bcdd36f44162b293487f1ca99
mystagogy <hi7747@naver.com>
2022-12-20 14:41:36 +0900

    feat : add mybatis

--- Admin/Admin/src/main/java/com/example/admin/AdminApplication.java
+++ Admin/Admin/src/main/java/com/example/admin/AdminApplication.java
@@ -1,9 +1,11 @@
 package com.example.admin;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(value={"com.example.admin.mapper"})
 public class AdminApplication {
 
     public static void main(String[] args) {
