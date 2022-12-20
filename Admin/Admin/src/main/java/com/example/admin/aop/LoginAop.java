a72f3269542d883777799fab3a60d6c5b8dd2527
mystagogy <hi7747@naver.com>
2022-12-20 15:27:32 +0900

    refactor : add aop for list controller

--- Admin/Admin/src/main/java/com/example/admin/aop/LoginAop.java
+++ Admin/Admin/src/main/java/com/example/admin/aop/LoginAop.java
@@ -16,12 +16,27 @@ public class LoginAop {
     @Pointcut("execution(* com.example.admin.controller.IndexController.*(..))")
     public void cut(){}
 
+    @Pointcut("execution(* com.example.admin.controller.ListController.*(..))")
+    public void cut2(){}
+
     @Around("cut()")
     public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
         HttpSession session = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest().getSession();
 
         if(session.getAttribute("id")==null){
             return 0;
+        }
+        else{
+            return joinPoint.proceed();
+        }
+    }
+
+    @Around("cut2()")
+    public Object around2(ProceedingJoinPoint joinPoint) throws Throwable {
+        HttpSession session = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest().getSession();
+
+        if(session.getAttribute("id")==null){
+            return null;
         }
         else{
             return joinPoint.proceed();
