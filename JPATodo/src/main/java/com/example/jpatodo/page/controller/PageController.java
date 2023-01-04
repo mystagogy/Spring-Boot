71d2ccee224ae5f160b34714f68de5ba8e11ea26
mystagogy <hi7747@naver.com>
2023-01-04 13:44:29 +0900

    feat : add index page and page controller

---
+++ JPATodo/src/main/java/com/example/jpatodo/page/controller/PageController.java
@@ -0,0 +1,9 @@
+package com.example.jpatodo.page.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PageController {
+
+    public String index(){ return "index"; }
+}
