a38bee512d7475b78cffe62c06589718dddf1a9f
mystagogy <hi7747@naver.com>
2023-01-09 14:12:29 +0900

    feat: create Entity
    for User, Schedule

---
+++ Calendar/src/main/java/com/jpa/calendar/entity/Schedule.java
@@ -0,0 +1,34 @@
+package com.jpa.calendar.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+@Entity
+@Table(name = "schedule")
+public class Schedule {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "date")
+    private String date;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "email", referencedColumnName = "email")
+    private User user;
+}
+
---
+++ Calendar/src/main/java/com/jpa/calendar/entity/User.java
@@ -0,0 +1,29 @@
+package com.jpa.calendar.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+@Entity
+@Table(name = "schedule_user")
+public class User {
+
+    @Id
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private Set<Schedule> scheduleList;
+}
